c77f1083f8115833fef2b6124fb570df9f031f9d
DudaC0d3r <118295787+DudaC0d3r@users.noreply.github.com>
2024-08-15 18:03:47 -0300

    erros e validaçoes

--- crud-spring/src/main/java/com/loiane/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/loiane/dto/CourseDTO.java
@@ -6,16 +6,16 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.loiane.enums.Category;
+import com.loiane.enums.validation.ValueOfEnum;
 
 public record CourseDTO(
         @JsonProperty("_id") Long id,
         @NotBlank @NotNull @Length(min = 5, max = 100) String name,
-        @NotNull @Length(max = 10, min = 0) @Pattern(regexp = "Back-end|Front-end") String category,
+        @NotNull @Length(max = 10) @ValueOfEnum(enumClass = Category.class) String category,
         @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
-
         }
